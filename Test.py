2298c6df686ba377135e9a1fb2604b40cb45f0c6
An Q Kieu <kieuan0@mccsc.edu>
2023-12-28 23:11:13 -0500

    check

--- Test.py
+++ Test.py
@@ -1,16 +1,16 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((1280,720), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
+
+screen.fill("light grey")
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill("green")
 
     pygame.display.flip()
 

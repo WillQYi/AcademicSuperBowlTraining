f9eea194f0253f752c813cd400430a00dc54e8f6
kieua <kieuanquoc@gmail.com>
2023-12-31 22:43:31 -0500

    element and sceen classes added

--- Main.py
+++ Main.py
@@ -1,29 +1,36 @@
 import pygame
+import elements
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720),pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
 
+'''
 def text(string, X, Y, size, color):
     font = pygame.font.Font('freesansbold.ttf', size)
     text = font.render(string, True, color)
     textRect = text.get_rect()
     textRect.center = (X,Y)
     screen.blit(text, textRect)
-
+'''
+    
 #Creates the homescreen
 def homescreen():
 
     darkBlue = (53, 63, 112)
 
     titleTextSize = 50
-    text("2023-2024 BHSS Academic Super Bowl", 640, 100, titleTextSize, darkBlue)
-    text("Math Training Tool", 640, 160, titleTextSize, darkBlue)
+    textDrawer = elements.TextDrawer(screen)
+    textDrawer.add("2023-2024 BHSS Academic Super Bowl", 640, 100, titleTextSize, darkBlue)
+    textDrawer.add("Math Training Tool", 640, 160, titleTextSize, darkBlue)
+    textDrawer.drawAll()
+    #text("2023-2024 BHSS Academic Super Bowl", 640, 100, titleTextSize, darkBlue)
+    #text("Math Training Tool", 640, 160, titleTextSize, darkBlue)
 
     buttonTextSize = 30
-    text("Training", 640, 350, buttonTextSize, darkBlue)
-    text("Resources", 640, 480, buttonTextSize, darkBlue)
+    #text("Training", 640, 350, buttonTextSize, darkBlue)
+    #text("Resources", 640, 480, buttonTextSize, darkBlue)
 
     pygame.draw.rect(screen, darkBlue, pygame.Rect(520, 300, 240, 100), 7, 10)
     pygame.draw.rect(screen, darkBlue, pygame.Rect(520, 430, 240, 100), 7, 10)
---
+++ Screens.py
@@ -0,0 +1,6 @@
+import pygame
+
+class homescreen:
+
+    def __init__(self):
+        pass

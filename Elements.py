6e4bd3159a9c587442b443ba5acdc2b4f01ba2d9
kieua <kieuanquoc@gmail.com>
2024-01-03 17:22:55 -0500

    getting ready for everything

--- Elements.py
+++ Elements.py
@@ -6,7 +6,6 @@ class TextDrawer:
     def __init__(self, screen):
         self.screen = screen
         self.drawTexts = []
-        pass
 
     def add(self, string, X, Y, size, color):
         self.drawTexts.append((string, X, Y, size, color))
@@ -21,7 +20,41 @@ class TextDrawer:
     def drawAll(self):
         for i in range(len(self.drawTexts)):
             self.drawOne(self.drawTexts[i][0],self.drawTexts[i][1],self.drawTexts[i][2],self.drawTexts[i][3],self.drawTexts[i][4])
-    
-        
 
-    
+# Creates a buttonObject which returns a event 
+# Note the drawing of the button is centerbased    
+class Button:
+
+    def __init__(self, screen, X, Y, sizeX, sizeY, color, thickness, curveRadius):
+        self.screen = screen
+        self.running = False
+        self.color = color
+        self.curveRadius = curveRadius
+        self.thickness = thickness
+        self.ButtonRect = pygame.Rect(X-sizeX/2, Y-sizeY/2, sizeX, sizeY)
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, self.ButtonRect, self.thickness, self.curveRadius) 
+
+    def clicked(self, mousePos):
+        if (self.ButtonRect.collidepoint(mousePos) and not self.running):
+            self.running = True
+            self.PressedAnimation()
+            return True
+        else:
+            return False
+        
+    def PressedAnimation(self):
+        print("Running...")
+        self.ButtonRect.scale_by(0.8)
+        self.draw()
+        '''
+        pygame.display.update()
+        pygame.time.wait(300)
+        self.ButtonRect.scale_by(1.25)
+        self.draw()
+        pygame.display.update()
+        '''
+        self.running = False
+        return
+        

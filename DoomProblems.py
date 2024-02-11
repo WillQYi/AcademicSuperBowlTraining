ad45001b78bb909c125eea457dacf0fd2df9f199
kieua <kieuanquoc@gmail.com>
2024-02-11 13:33:58 -0500

    Doomsday problems worked on

--- DoomProblems.py
+++ DoomProblems.py
@@ -113,7 +113,7 @@ class DoomProblem1:
         elif (choice <= 100):
             self.year = 400 * random.randint(1,10)
 
-        self.anchorDay = (2 + self.year + math.floor(self.year/4) + math.floor(self.year/100) + math.floor(self.year/400))%7
+        self.anchorDay = (2 + self.year + math.floor(self.year/4) - math.floor(self.year/100) + math.floor(self.year/400))%7
 
         pass
 
@@ -176,67 +176,172 @@ class DoomProblem2:
         self.answers = []
         self.answerInputText = []
 
-        self.generateProblem()
+        self.generateProblem("null")
         self.generateQuestionAndAnswer()
     
-    def generateProblem(self):
+    def generateProblem(self, test):
         
-        #Generate year
-        choice = random.randint(1,100)
-        if (choice <= 65):
-            self.year = random.randint(1,4000)
-        elif (choice <= 85):
-            self.year = 4 * random.randint(1,500)
-        elif (choice <= 95):
-            self.year = 100 * random.randint(1,40)
-        elif (choice <= 100):
-            self.year = 400 * random.randint(1,10)
+        try:
+            self.month = int(test.split("/")[0])-1
+            self.day = int(test.split("/")[1])
+            self.year = int(test.split("/")[2])
+
+            #Check for leap year
+            self.isLeapYear = False
+            if (self.year % 400 == 0 or self.year % 4 == 0 and self.year % 100 != 0):
+                self.isLeapYear = True
+
+        except:
+
+            #Generate year
+            choice = random.randint(1,100)
+            if (choice <= 65):
+                self.year = random.randint(1,4000)
+            elif (choice <= 85):
+                self.year = 4 * random.randint(1,500)
+            elif (choice <= 95):
+                self.year = 100 * random.randint(1,40)
+            elif (choice <= 100):
+                self.year = 400 * random.randint(1,10)
+
+            #Check for leap year
+            self.isLeapYear = False
+            if (self.year % 400 == 0 or self.year % 4 == 0 and self.year % 100 != 0):
+                self.isLeapYear = True
+
+            #Generate the month
+            self.month = random.randint(0,11)
+            #print("Month: " + str(DoomProblems.monthNames[self.month]))
+
+            #Generate Day
+            if (self.isLeapYear and self.month == 1):
+                self.day = random.randint(1,DoomProblems.numberOfDaysInMonth[1]+1)
+            else:
+                self.day = random.randint(1,DoomProblems.numberOfDaysInMonth[self.month])
+
 
         #Find the anchor day
-        self.anchorDay = (2 + self.year + math.floor(self.year/4) + math.floor(self.year/100) + math.floor(self.year/400))%7
-        print("Anchor day: " + str(DoomProblems.days[self.anchorDay]))
-
-        #Generate the month
-        self.month = random.randint(0,11)
-        print("Month: " + str(DoomProblems.monthNames[self.month]))
-
-        #Check for leap year
-        self.isLeapYear = False
-        if (self.year % 400 == 0 or self.year % 4 == 0 and self.year % 100 != 0):
-            self.isLeapYear = True
-
-        #Generate Day
-        if (self.isLeapYear and self.month == 1):
-            self.day = random.randint(1,DoomProblems.numberOfDaysInMonth[1]+1)
-        else:
-            self.day = random.randint(1,DoomProblems.numberOfDaysInMonth[self.month])
+        self.anchorDay = (2 + self.year + math.floor(self.year/4) - math.floor(self.year/100) + math.floor(self.year/400))%7
+        #print("Anchor day: " + str(DoomProblems.days[self.anchorDay]))
 
         #Find the day of the week the day is on
-        if (self.isLeapYear and self.month == 0 or self.month == 1):
-            print("Doomsday of month: " + str(DoomProblems.doomsDays[self.month]+1))
+        if (self.isLeapYear and (self.month == 0 or self.month == 1)):
+            #print("Doomsday of month: " + str(DoomProblems.doomsDays[self.month]+1))
             self.daysAway = (self.day-(DoomProblems.doomsDays[self.month]+1))%7
         else:
-            print("Doomsday of month: " + str(DoomProblems.doomsDays[self.month]))
+            #print("Doomsday of month: " + str(DoomProblems.doomsDays[self.month]))
             self.daysAway = (self.day-(DoomProblems.doomsDays[self.month]))%7
 
-        print("Days away: " + str(self.daysAway))
-
-        if (self.daysAway < 0):
-            self.daysAway += 7
+        #print("Days away: " + str(self.daysAway))
 
         self.answer = (self.anchorDay + self.daysAway)%7
-        
+
+        print("Day: " + str(DoomProblems.days[self.answer]))
+
         pass
 
     def generateQuestionAndAnswer(self):
     
         self.answers = DoomProblems.dayInputs[self.anchorDay]
 
-        print(DoomProblems.days[self.anchorDay])
+        #print(DoomProblems.days[self.anchorDay])
 
         self.question = []
 
         question = "What day is " + str(self.month+1) + "/" + str(self.day) + "/" + str(self.year) + " in the Gregorian Calender"
+
+        self.question.append(question)
+
+        pass
+
+    def checkCorrect(self, answer = str):
+
+        reformatedAnswer = (answer.upper()).lower()
+
+        if (reformatedAnswer in self.answers):
+            return True
+
+        return False
+
+    def testAlgorithm(self, test):
+
+        month = int(test.split("/")[0])
+        day = int(test.split("/")[1])
+        year = int(test.split("/")[2])
+
+        isLeapYear = False
+        if (year % 400 == 0 or year % 4 == 0 and year % 100 != 0):
+            isLeapYear = True
+
+        anchorDay = (2 + year + math.floor(year/4) - math.floor(year/100) + math.floor(year/400))%7
+
+        doomsDay = DoomProblems.doomsDays[month]
+
+        daysUntil = (day-doomsDay)%7
+
+        day = (anchorDay + daysUntil)%7
+
+        return day
+
+    def getQuestion(self):
+        return self.question
+
+    def getAnswer(self):
+        return self.answers
+        
+    def display(self):
+        pass
+
+
+#Find the anchor day for a given year in the Julian calcender
+class DoomProblem3: 
+
+    def __init__(self):
+
+        self.question = []
+        self.answers = []
+        self.answerInputText = []
+
+        self.problemDisplayType = "lines"
+
+        self.create()
+
+        pass
+
+    def create(self):
+
+        self.question = []
+        self.answers = []
+        self.answerInputText = []
+
+        self.generateProblem()
+        self.generateQuestionAndAnswer()
+    
+    def generateProblem(self):
+        
+        choice = random.randint(1,100)
+        if (choice <= 75):
+            self.year = random.randint(1,4000)
+        elif (choice <= 100):
+            self.year = 4 * random.randint(1,500)
+
+        self.anchorDay = (self.year + math.floor(self.year/4))%7
+        pass
+
+    def testAlgorithm(self,test):
+
+        return (test + math.floor(test/4))%7
+
+
+    def generateQuestionAndAnswer(self):
+    
+        self.answers = DoomProblems.dayInputs[self.anchorDay]
+
+        #print(DoomProblems.days[self.anchorDay])
+
+        self.question = []
+
+        question = "Find the anchor day of the year " + str(self.year) + " in the Julian Calender"
 
         self.question.append(question)
 
@@ -256,12 +361,11 @@ class DoomProblem2:
 
     def getAnswer(self):
         return self.answers
-        
-    def display(self):
-        pass
 
-
-test = DoomProblem1()
-print("Test: " + str(test.testAlgorithm(1800)))
-print(test.getQuestion())
-print(test.getAnswer())
+Jtest = DoomProblem3()
+Gtest = DoomProblem1()
+#print("Test: " + str(DoomProblems.days[test.generateProblem("2/5/2024")]))
+print("GTest: " + str(DoomProblems.days[Gtest.testAlgorithm(2015)]))
+print("JTest: " + str(DoomProblems.days[Jtest.testAlgorithm(2015)]))
+#print(test.getQuestion())
+#print(test.getAnswer())
